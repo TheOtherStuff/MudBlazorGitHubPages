6a848e942f45c15d49c477e6140555205032f65f
Jerome Bell <jeromebell@gmail.com>
2024-08-31 11:54:38 -0500

    Refactor

--- src/MudBlazorPages/Program.cs
+++ src/MudBlazorPages/Program.cs
@@ -3,12 +3,11 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
-using static System.Net.WebRequestMethods;
 using System.Text.Json.Serialization;
 using MudBlazorPages;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<MudBlazorPages.App>("#app");
+builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 var http = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
